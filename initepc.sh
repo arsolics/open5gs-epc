08c6b96510525df99e26693d08e9d15739d4f89f
solics <solics@gmail.com>
2021-04-29 18:47:43 +0300

    init script

---
+++ initepc.sh
@@ -0,0 +1,6 @@
+docker pull arsolics/open5gs:1618491821
+docker tag arsolics/open5gs:1618491821 docker_open5gs:latest
+docker-compose up -d
+docker cp sims/sims.json mongo:/mnt/sims.json
+docker exec -it mongo mongoimport -h 100.77.0.2 -d open5gs -c subscribers /mnt/sims.json
+
