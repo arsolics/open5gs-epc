6c95de87001b4ddc73806a2f1f6ac7f3bffc18fd
solics <solics@legba.ro>
2021-12-22 04:38:11 -0500

    sims.json's right path

--- initepc.sh
+++ initepc.sh
@@ -2,7 +2,7 @@ urpmi --force docker docker-compose
 docker pull arsolics/open5gs:1618491821
 docker tag arsolics/open5gs:1618491821 docker_open5gs:latest
 docker-compose up -d
-docker cp sims.json mongo:/mnt/sims.json
+docker cp sims/sims.json mongo:/mnt/sims.json
 #docker exec -it mongo mongoimport -h 100.77.0.2 -d open5gs -c subscribers /mnt/sims.json
 bash mongoinit.sh
 bash yate-sdr-fix.sh
