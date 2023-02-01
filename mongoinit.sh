cba150c44a5fb87584ed6ef6c12cb58b9123b06a
Your Name <you@example.com>
2023-02-02 01:43:04 +0200

    improvements

--- mongoinit.sh
+++ mongoinit.sh
@@ -27,7 +27,7 @@ echo "{ 'imsi' : '$IMSI', 'pdn': [ $myap ] , 'ambr' : { 'downlink' : '512000' , 
 
 
 
-done < ./sims/sims.csv > sims.json
+done < ./sims/sims.csv > sims/sims.json
 echo "db.subscribers.remove( { } )" | docker exec -i mongo mongo 100.77.0.2/open5gs --
 docker cp sims/sims.json mongo:/mnt/sims.json
 docker exec -it mongo mongoimport -h 100.77.0.2 -d open5gs -c subscribers /mnt/sims.json
